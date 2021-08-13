9f6400482afc2a1e612b35cd68af4dd1b1afec3a
illker <illker@hotmail.com>
2021-08-12 22:55:38 -0500

    Minimum Operations :sunglasses:

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,19 +4,14 @@
 
 def minOperations(n):
     """calculates the fewest number of operations needed"""
-    number = 1
-    copy, = 0
-    operations = 0
-    if type(n) is not int or n < 2:
+    if n <= 1:
         return 0
-    else:
-        for i in range(n):
-            if number == n:
-                return operations
-            elif n % number == 0:
-                copy = number
-                number += copy
-                operations += 2
-            else:
-                number += copy
-                operations += 1
+    num, div, numOfOperations = n, 2, 0
+
+    while num > 1:
+        if num % div == 0:
+            num = num / div
+            numOfOperations = numOfOperations + div
+        else:
+            div += 1
+    return numOfOperations
