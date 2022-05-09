ed872d3f7f857572ceb606c730da69d188bfd190
illker <illker@hotmail.com>
2022-05-08 23:51:52 -0500

    🙎‍♀️🥲😇👈🧑‍🏫🧑‍🏭🧦🐂😅

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,46 +1,53 @@
 #!/usr/bin/python3
-"""0-count module"""
+"""
+Count it!
+"""
+from requests import request
 
-import collections
-import requests
 
-
-def fill_list(subreddit, hot_list=[], after=None):
-    """Create list of words in hot titles of specified subreddit
+def generate_dicts(word_list):
     """
-    req = requests.get("https://www.reddit.com/r/{}/hot.json?after={}".
-                       format(subreddit, after),
-                       headers={"User-agent": "agent"},
-                       allow_redirects=False)
-    if req.status_code != 200:
-        return None
-    after = req.json().get("data").get("after")
-    if after is None:
-        return hot_list
-    for i in req.json().get("data").get("children"):
-        for word in i.get("data").get("title").split():
-            hot_list.append(word.lower())
-    return fill_list(subreddit, hot_list, after)
-
-
-def count_words(subreddit, word_list):
-    """Finds occurences of specified keywords
+    generate_dicts functions
     """
-    if subreddit is None or subreddit == "" or word_list is None:
+    count = {k: 0 for k in word_list}
+    dup = {}
+    for k in word_list:
+        if k not in dup:
+            dup[k] = 0
+        dup[k] += 1
+    return (count, dup)
+
+
+def count_words(subreddit, word_list, after="", count={}, dup={}, init=0):
+    """
+    count_words function
+    """
+    if not init:
+        count, dup = generate_dicts(word_list)
+
+    url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, after)
+    headers = {"User-Agent": "Python3"}
+    response = request("GET", url, headers=headers).json()
+    try:
+        data = response.get('data')
+        top = data.get('children')
+        _after = data.get('after')
+
+        for item in top:
+            data = item.get('data')['title']
+            for word in count:
+                amount = data.lower().split(' ').count(word.lower())
+                count[word] += amount
+
+        if _after:
+            count_words(subreddit, word_list, _after, count, dup, 1)
+        else:
+            sort_abc = sorted(count.items(), key=lambda tup: tup[::-1])
+            desc = sorted(sort_abc, key=lambda tup: tup[1], reverse=True)
+
+            for name, cnt in desc:
+                cnt *= dup[name]
+                if cnt:
+                    print('{}: {}'.format(name.lower(), cnt))
+    except Exception:
         return None
-    hot_list = fill_list(subreddit)
-    if hot_list is None:
-        return None
-    all_cnt = collections.Counter(hot_list)
-    filtered_cnt = {}
-    for word in word_list:
-        word_l = word.lower()
-        if all_cnt[word_l] > 0:
-            if word in filtered_cnt:
-                filtered_cnt[word] += filtered_cnt[word]
-            else:
-                filtered_cnt[word] = all_cnt[word_l]
-    for k, v in sorted(filtered_cnt.items(),
-                       key=lambda item: item[1], reverse=True):
-        print("{}: {}".format(k, v))
-    return filtered_cnt
